9eec2a6e77f11c187ffd6abb2d1fb2622b9ecc8b
yun9699 <cha23215@daum.net>
2024-08-19 18:29:26 +0900

    0819

---
+++ src/components/Header.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="header">
+        <div class="head">
+          <h1>
+            <a href="#">SleepEyes</a>
+          </h1>
+          <div class="pc_nav">
+            <div class="bg"></div>
+            <ul>
+              <li>
+                <a href="./sub.html">Brand</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">개발배경</a></li>
+                    <li><a href="./sub.html">연구분야</a></li>
+                    <li><a href="./sub.html">기업소개</a></li>
+                    <li><a href="./sub.html">비즈니스 파트너</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a href="./sub.html">Technology</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">SleepEyes란</a></li>
+                    <li><a href="./sub.html">수면진단 알고리즘</a></li>
+                    <li><a href="./sub.html">딥러닝 음성인식 알고리즘</a></li>
+                    <li><a href="./sub.html">레이더 센싱 기술</a></li>
+                    <li><a href="./sub.html">빅데이터</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a href="./sub.html">Product</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">SleepEyes란</a></li>
+                    <li><a href="./sub.html">JadaIn이란</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a href="./sub.html">Customer</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">언론보도</a></li>
+                    <li><a href="./sub.html">미디어</a></li>
+                    <li><a href="./sub.html">AS안내</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="gnb">
+            <ul class="btn1">
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+            <div class="language">
+              <a>KOR <i></i></a>
+              <a>ENG</a>
+            </div>
+            <div class="btnmenu">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div class="allmenu">
+          <div>
+            <div class="menuwrap">
+              <div class="logoarea">
+                <img src="/images/common/all_logo.png" alt="" />
+              </div>
+              <div class="menuarea">
+                <ul>
+                  <li>
+                    <a>BRAND</a>
+                    <ul>
+                      <li><a href="./sub.html">개발배경</a></li>
+                      <li><a href="./sub.html">연구분야</a></li>
+                      <li><a href="./sub.html">기업소개</a></li>
+                      <li><a href="./sub.html">비즈니스 파트너</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>TECHNOLOGY</a>
+                    <ul>
+                      <li><a href="./sub.html">SleepEyes란</a></li>
+                      <li><a href="./sub.html">수면진단 알고리즘</a></li>
+                      <li><a href="./sub.html">딥러닝 음성인식 알고리즘</a></li>
+                      <li><a href="./sub.html">레이더 센싱 기술</a></li>
+                      <li><a href="./sub.html">빅데이터</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>PRODUCT</a>
+                    <ul>
+                      <li><a href="./sub.html">SleepEyes소개</a></li>
+                      <li><a href="./sub.html">JadaIn소개</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>CUSTOMER</a>
+                    <ul>
+                      <li><a href="./sub.html">언론보도</a></li>
+                      <li><a href="./sub.html">미디어</a></li>
+                      <li><a href="./sub.html">AS안내</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Sample.vue
@@ -0,0 +1,461 @@
+<template>
+  <!-- <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SenSolution</title>
+    <link rel="stylesheet" href="./css/reset.css" />
+    <link rel="stylesheet" href="./css/common_pc.css" />
+    <link rel="stylesheet" href="./css/common_tablet.css" />
+    <link rel="stylesheet" href="./css/common_mobile.css" />
+    <link rel="stylesheet" href="./css/main_pc.css" />
+    <link rel="stylesheet" href="./css/main_tablet.css" />
+    <link rel="stylesheet" href="./css/main_mobile.css" />
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.min.js"></script>
+    <script src="./js/common.js"></script>
+    <script src="./js/main.js"></script>
+  </head> -->
+  <body>
+    <div id="wrap">
+      <div id="header">
+        <div class="head">
+          <h1>
+            <a href="#">SleepEyes</a>
+          </h1>
+          <div class="pc_nav">
+            <div class="bg"></div>
+            <ul>
+              <li>
+                <a href="./sub.html">Brand</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">개발배경</a></li>
+                    <li><a href="./sub.html">연구분야</a></li>
+                    <li><a href="./sub.html">기업소개</a></li>
+                    <li><a href="./sub.html">비즈니스 파트너</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a href="./sub.html">Technology</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">SleepEyes란</a></li>
+                    <li><a href="./sub.html">수면진단 알고리즘</a></li>
+                    <li><a href="./sub.html">딥러닝 음성인식 알고리즘</a></li>
+                    <li><a href="./sub.html">레이더 센싱 기술</a></li>
+                    <li><a href="./sub.html">빅데이터</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a href="./sub.html">Product</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">SleepEyes란</a></li>
+                    <li><a href="./sub.html">JadaIn이란</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a href="./sub.html">Customer</a>
+                <div class="sub">
+                  <ul>
+                    <li><a href="./sub.html">언론보도</a></li>
+                    <li><a href="./sub.html">미디어</a></li>
+                    <li><a href="./sub.html">AS안내</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="gnb">
+            <ul class="btn1">
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+            <div class="language">
+              <a>KOR <i></i></a>
+              <a>ENG</a>
+            </div>
+            <div class="btnmenu">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div class="allmenu">
+          <div>
+            <div class="menuwrap">
+              <div class="logoarea">
+                <img src="/images/common/all_logo.png" alt="" />
+              </div>
+              <div class="menuarea">
+                <ul>
+                  <li>
+                    <a>BRAND</a>
+                    <ul>
+                      <li><a href="./sub.html">개발배경</a></li>
+                      <li><a href="./sub.html">연구분야</a></li>
+                      <li><a href="./sub.html">기업소개</a></li>
+                      <li><a href="./sub.html">비즈니스 파트너</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>TECHNOLOGY</a>
+                    <ul>
+                      <li><a href="./sub.html">SleepEyes란</a></li>
+                      <li><a href="./sub.html">수면진단 알고리즘</a></li>
+                      <li><a href="./sub.html">딥러닝 음성인식 알고리즘</a></li>
+                      <li><a href="./sub.html">레이더 센싱 기술</a></li>
+                      <li><a href="./sub.html">빅데이터</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>PRODUCT</a>
+                    <ul>
+                      <li><a href="./sub.html">SleepEyes소개</a></li>
+                      <li><a href="./sub.html">JadaIn소개</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>CUSTOMER</a>
+                    <ul>
+                      <li><a href="./sub.html">언론보도</a></li>
+                      <li><a href="./sub.html">미디어</a></li>
+                      <li><a href="./sub.html">AS안내</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- //header -->
+      <div id="contents">
+        <div class="section" id="section1">
+          <div class="container visual">
+            <div class="wrapper">
+              <div class="slide v1">
+                <div class="bg">
+                  <video muted autoplay loop playsinline>
+                    <source src="/images/intro3.mp4" type="video/mp4" />
+                  </video>
+                </div>
+              </div>
+            </div>
+            <div class="txtwrap">
+              <h2>수면 최적화 AI 솔루션</h2>
+              <p>
+                딥러닝 알고리즘 기법으로 나만의 최적화된 수면 솔루션을
+                제공합니다.
+              </p>
+            </div>
+            <div class="ctr">
+              <div class="vprev"></div>
+              <div class="pagination">
+                <span></span>
+              </div>
+              <div class="vnext"></div>
+            </div>
+            <div class="scroll">
+              <img src="/images/main/scrolldown.png" alt="Scroll Down" />
+              <div class="line"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="section" id="section2">
+          <div class="conwrap">
+            <div class="titlearea">
+              <div>
+                <h2>Technology</h2>
+                <h3>
+                  비접촉 레이더 센서 방식의
+                  <span class="pcbr"></span>
+                  새로운 수면 패러다임
+                </h3>
+                <a class="btn_more" href="./sub.html">MORE VIEW</a>
+              </div>
+            </div>
+            <div class="tech bg1">
+              <div class="txtwrap">
+                <div class="tit">SleepEyes 란?</div>
+              </div>
+            </div>
+            <div class="tech bg2">
+              <div class="txtwrap">
+                <div class="tit">수면진단 알고리즘</div>
+              </div>
+            </div>
+            <div class="tech bg3">
+              <div class="txtwrap">
+                <div class="tit">레이더 센싱 기술</div>
+              </div>
+            </div>
+            <div class="tech bg4">
+              <div class="txtwrap">
+                <div class="tit">딥러닝 음성인식 알고리즘</div>
+              </div>
+            </div>
+            <div class="tech bg5">
+              <div class="">
+                <div class="tit">
+                  나만의 수면패턴<br />
+                  똑똑하게 알려주다!
+                </div>
+              </div>
+            </div>
+            <div class="tech bg6">
+              <div class="txtwrap">
+                <div class="tit">빅데이터</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="section" id="section3">
+          <div class="conwrap">
+            <div class="txtwrap">
+              <div class="titlearea">
+                <div>
+                  <h2>Product</h2>
+                  <h3>
+                    SLEEP EYES와 함께
+                    <span class="pcbr"></span>
+                    스마트한 일상을 만들어보세요.
+                  </h3>
+                  <a class="btn_more more2" href="./sub.html">MORE VIEW</a>
+                </div>
+              </div>
+              <div class="merit">
+                <dl>
+                  <dt>초정밀수면진단</dt>
+                  <dd>
+                    수면패턴 관리로 깊은 수면을 취하고
+                    <span class="pcbr"></span>
+                    수면장애를 개선 할 수 있습니다.
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>비접촉방식</dt>
+                  <dd>
+                    수면에 방해를 주지않는 레이더
+                    <span class="pcbr"></span>
+                    센싱기술을 도입하였습니다.
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>수면무호흡증 진단 및 알림</dt>
+                  <dd>
+                    수면무호흡 발생 10초후 알람을
+                    <span class="pcbr"></span>
+                    제공 합니다.
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>게스트모드</dt>
+                  <dd>
+                    자녀와 부모님의 슬립 컨디션을
+                    <span class="pcbr"></span>
+                    확인 해보세요.
+                  </dd>
+                </dl>
+              </div>
+            </div>
+            <div class="imgwrap">
+              <img src="/images/main/sec3_img1.png" alt="" class="img1" />
+              <img src="/images/main/sec3_img2.png" alt="" class="img2" />
+              <img src="/images/main/sec3_img3.png" alt="" class="img3" />
+            </div>
+          </div>
+        </div>
+
+        <div class="section" id="section4">
+          <div class="conwrap">
+            <div class="titlearea">
+              <div>
+                <h2>Media</h2>
+                <h3>슬립아이즈 기업정보 및 제품정보 영상입니다.</h3>
+                <a href="/sub.html" class="btn_more more2">MORE VIEW</a>
+              </div>
+            </div>
+            <div class="nn">
+              <div class="container notice">
+                <div class="wrapper">
+                  <div class="slide">
+                    <a href="https://youtu.be/KgRXGXnmgXE" target="_blank">
+                      <div class="thumb">
+                        <img
+                          src="/images/main/file_thumb_1691715180ygf7auplgs.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="info">
+                        <div>
+                          <div class="tit">
+                            슬립테크 2023 국제수면 건강박람회 참가 인터뷰
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="slide">
+                    <a href="https://youtu.be/nBUdNR0U2VM" target="_blank">
+                      <div class="thumb">
+                        <img
+                          src="/images/main/file_thumb_1683861819yhiewiccs7.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="info">
+                        <div>
+                          <div class="tit">
+                            수면을 진단하여 깊은 잠을 이루다.
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="slide">
+                    <a href=" https://youtu.be/4cN2UOp-lO4" target="_blank">
+                      <div class="thumb">
+                        <img
+                          src="/images/main/file_thumb_1682294188fvkf2xvznz.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="info">
+                        <div>
+                          <div class="tit">레이더 센싱 기술</div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="slide">
+                    <a href="https://youtu.be/VJ7xmjmzAGQ" target="_blank">
+                      <div class="thumb">
+                        <img
+                          src="/images/main/file_thumb_1682294316gdzyg6jek1.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="info">
+                        <div>
+                          <div class="tit">수면무호흡 알람</div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="slide">
+                    <a href=" https://youtu.be/p2A8UySXylg" target="_blank">
+                      <div class="thumb">
+                        <img
+                          src="/images/main/file_thumb_1682294914faxpws8ow4.png"
+                          alt=""
+                        />
+                      </div>
+                      <div class="info">
+                        <div>
+                          <div class="tit">SleepEyes is...</div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="slide">
+                    <a href="https://youtu.be/ooSFQfp3OtA" target="_blank">
+                      <div class="thumb">
+                        <img
+                          src="/images/main/file_thumb_16822952761s340t67bh.png"
+                          alt=""
+                        />
+                      </div>
+                      <div class="info">
+                        <div>
+                          <div class="tit">UWB Radar Module [ 호흡감지 ]</div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="btn_top">
+          <a><img src="/images/common/btn_top.png" alt="" /></a>
+        </div>
+      </div>
+      <!-- //contents -->
+      <div id="footer">
+        <div class="foot">
+          <h1><img src="/images/common/foot_logo.png" alt="" /></h1>
+          <div class="info">
+            <div class="contact">
+              <dl>
+                <dt>Address.</dt>
+                <dd>경기도 하남시 조정대로 150, 5층 569호(덕풍동, 아이테코)</dd>
+              </dl>
+              <dl>
+                <dt>Tel.</dt>
+                <dd>031-790-0507</dd>
+              </dl>
+              <dl>
+                <dt>Email.</dt>
+                <dd>sales@sensolution.co.kr</dd>
+              </dl>
+            </div>
+            <div class="copy">
+              Copyright ⓒ 2022 SENSOLUTION. All Rights Reseved.
+            </div>
+          </div>
+          <ul>
+            <li><a>기업소개</a></li>
+            <li><a>개인정보처리방침</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- //footer -->
+    </div>
+    <!-- //wrap -->
+    <!-- <div id="fp-nav" class="right" style="margin-top: -101.5px">
+      <ul>
+        <li>
+          <a href="#" class="active">
+            <span></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span></span>
+          </a>
+        </li>
+      </ul>
+    </div> -->
+  </body>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
